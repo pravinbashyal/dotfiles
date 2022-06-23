18639a7cbcb1fcc0d375d877e39e2c73e930148b
Pravin Bashyal <pravin.bashyal@signavio.com>
2022-06-23 15:31:17 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-gem install tmuxinator
-
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
